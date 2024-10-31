4492bffbf532e4af0cd536f9b1c17f21b590d36d
Mahmoud Mostafa <meropvo17@gmail.com>
2024-10-31 23:55:35 +0200

    Create Date Input

---
+++ src/components/UI/Inputs/DateInput.tsx
@@ -0,0 +1,28 @@
+import React, { useRef, useState } from 'react';
+import classes from './DateInput.module.scss';
+import calenderIcon from "../../../assets/icons/calender.png"
+interface props {
+    placeholder: string
+    name: string
+}
+const DateInput: React.FC<props> = ({ placeholder, name }) => {
+    const [date, setDate] = useState<string>("");
+    const datePicker = useRef<HTMLInputElement>(null);
+    function handleClick() {
+        datePicker.current?.showPicker();
+    }
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        setDate(e.target.value);
+    }
+    return (
+        <div className={classes.dateContainer}>
+            <input type="date" name={name} onChange={handleInput} value={date} ref={datePicker} className={classes.dateButton + " " + classes.actualInput} />
+            <input type='text' readOnly placeholder={placeholder} value={date} className={classes.dateInput} />
+            <button onClick={handleClick} className={classes.dateButton}>
+                <img src={calenderIcon} />
+            </button>
+        </div>
+    );
+}
+
+export default DateInput;
